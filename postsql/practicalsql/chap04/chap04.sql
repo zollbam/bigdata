559ba1a8993d6b02903897f49f5b4ef7948b8e27
zollbam <whrjsdud93@naver.com>
2023-04-04 01:08:44 +0900

    230404commit

--- postsql/practicalsql/chap04/chap04.sql
+++ postsql/practicalsql/chap04/chap04.sql
@@ -1,4 +1,4 @@
--- Active: 1679909868066@@127.0.0.1@5432@postgres
+-- Active: 1679746005886@@127.0.0.1@5432@gydb
 -- 데이터 타입
 -- * 1. char(n)
 /*
@@ -96,6 +96,99 @@ create table people (
     id integer generated always as identity,
     person_name varchar(100)
 );
+
+-- * 10. timestamp
+/*
+- 8바이트, 날짜와 시간
+- timestamptz로 timestamp with time zone타입과 동일한 데이터 타입을 지정
+*/
+
+-- * 11. date
+/*
+- 4바이트, 날짜
+*/
+
+-- * 12. time
+/*
+- 8바이트, 시간
+*/
+
+-- * 13. interval
+/*
+- 16바이트, 시간차이
+- 기간의 시작 또는 끝은 기록하지 않고 길이만 기록
+*/
+
+-- 테이블 생성
+create table date_time_types(
+    timestamp_column timestamp with time zone,
+    interval_column interval
+);
+
+-- 데이터 삽입
+insert into date_time_types values 
+    ('2022-12-31 01:00 EST', '2 days'), -- EST는 미국 동부 표준시의 약어
+    ('2022-12-31 01:00 -8', '1 month'), -- UTC의 시간 차이 또는 오프셋 수를 나타냄 = > -8: 미국과 캐나다의 태평양 표준 시간
+    ('2022-12-31 01:00 Australia/Melbourne', '1 century'), -- century은 100년
+    (now(), '1 week');
+
+-- 테이블 확인
+table date_time_types;
+
+-- 날짜 시간 계산
+SELECT timestamp_column,
+       interval_column,
+       timestamp_column - interval_column as new_date
+from date_time_types;
+
+-- * 14. json
+/*
+- JSON 텍스트 그대로 저장
+*/ 
+
+-- * 15. jsonb
+/*
+- JSON 텍스트를 바이너리 형식으로 변환해서 저장
+- 인덱싱을 지원하므로 처리속도가 빠름
+*/ 
+
+-- * 16. boolean
+/*
+- true 또는 false 값을 저장하는 논리 타입
+*/ 
+
+-- * 17. geometric
+/*
+- 점, 선, 원 및 기타 2차원 개체를 포함하는 기하 타입
+*/ 
+
+-- * 18. text search
+/*
+- postgresql 전문 검색 엔진용 텍스트 검색 타입
+*/ 
+
+-- * 19. network address
+/*
+- IP 또는 MAC 주소와 같은 네트워크 주소 타입
+*/ 
+
+-- * 20. Universally Unique Idenetifier
+/*
+- 경우에 따라 테이블의 고유키로 사용될 수 있는 UUID타입
+*/ 
+
+-- * 21. range
+/*
+- 정수 또는 타임스탬프 같은 값의 범위를 지정하는 범위 타입
+*/ 
+
+-- * 22. 그 외
+/*
+- 바이너리 데이터를 저장하는 타입
+- 구조화된 형식으로 정보를 저장하는 XML 및 JSON타입
+*/ 
+
+
 
 
 
