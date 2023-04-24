6e17f74c50391f0ed4dd754a0211b78295f3f809
zollbam <whrjsdud93@naver.com>
2023-04-24 10:56:17 +0900

    230424commit

---
+++ mssql/youtube/youngju/user_function.sql
@@ -0,0 +1,105 @@
+﻿-- 스칼라 함수
+create function f_plus(
+	@value1 int,
+	@value2 int
+)
+returns int
+as 
+begin
+declare @sum_value int
+set @sum_value=(@value1+@value2)
+return @sum_value
+end;
+
+go
+select dbo.f_plus(12,14);
+
+use study
+go 
+select * from stockprice;
+
+select * from companyinfo;
+
+create function f_return( -- 수익률 계산
+	@startdate date,
+	@enddate date,
+	@id int
+)
+returns decimal(12,10)
+as 
+begin
+declare @startprice numeric
+declare @endprice numeric
+
+set @startdate = (select min(date_) from stockprice where id = @id and date_>=@startdate)
+set @enddate = (select max(date_) from stockprice where id = @id and date_<=@enddate)
+set @startprice = (select close_ from stockprice where id = @id and date_=@startdate)
+set @endprice = (select min(close_) from stockprice where id = @id and date_=@enddate)
+return (@endprice/@startprice - 1.0)
+end;
+
+drop function f_return;
+
+select *, dbo.f_return('2020-10-01', '2020-10-12', ID) "ret"
+from companyinfo
+order by "ret" desc;
+
+-- 테이블 함수
+create function f_info(
+	@id int
+)
+returns table
+as 
+return(
+	select R.FIN_NAME,
+	       R.FIN_VAL,
+		   R.FDATE,
+		   R.FPRD,
+		   A.NAME
+	from companyinfo A
+	     join IDMap I
+			on A.ID = I.ID 
+	     join Ratios R
+			on I.FIN_ID=R.FIN_ID
+	where A.ID=@id
+)
+go 
+select *
+from dbo.f_info(40853);
+
+create function f_info2(
+	@id int
+)
+returns @return_table table(
+	FIN_NAME varchar(300), 
+	FIN_VAL float,
+	FDATE date,
+	FPRD int,
+	Name nvarchar(3000)
+)
+as
+begin
+insert into @return_table
+	select R.FIN_NAME,
+	       R.FIN_VAL,
+		   R.FDATE,
+		   R.FPRD,
+		   A.NAME
+	from companyinfo A
+	     join IDMap I 
+			on a.id=i.id 
+		 join Ratios R
+			on i.FIN_ID=R.FIN_ID
+	where A.ID=@ID
+return
+end
+go 
+select *
+from dbo.f_info2(40853);
+
+-- 함수 수정 및 삭제
+-- 1) 삭제
+drop function f_info2;
+
+-- 2) 수정
+alter function 
---
+++ mssql/youtube/youngju/user_procedure.sql
@@ -0,0 +1,104 @@
+-- 저장 프로시저 저장
+use study
+go
+create procedure SP_SELECT_COMPANYINFO
+as
+begin
+	select * from companyinfo 
+end
+go
+exec SP_SELECT_COMPANYINFO;
+
+-- 매개변수가 있는 프로시저
+create procedure SP_SELECT_STOCKPRICE 
+@ID INT,
+@STARTDATE DATE,
+@ENDDATE DATE
+as
+begin
+	select Date_,close_
+	from stockprice 
+	where id = @id and
+	      date_ between @startdate and @enddate
+	order by date_ desc
+end
+go
+exec SP_SELECT_STOCKPRICE 40853, '2020-10-01', '2020-10-13';
+
+drop proc SP_SELECT_STOCKPRICE;
+
+-- 프로시저 수정 및 삭제
+-- 1) 수정
+/*
+alter procedure [프로시저명]
+@[파라미터명]
+as
+begin
+[쿼리문]
+end
+*/
+
+-- 2)삭제
+/*
+drop proc [프로시저명]
+*/
+
+-- if else 문
+declare @I int =111
+if @I%2 = 0
+	begin
+		print N'짝수'
+	end
+else 
+	begin
+		print N'홀수'
+	end;
+
+-- while문
+declare @I int = 1 
+declare @added bigint = 0
+while @I <= 100 -- (@I <= 100)로 괄호로 묶어도 실행 가능
+	begin
+		set @added = @added + @I
+		print N'현재 숫자는 ' + cast(@I as varchar) + N'이고 총 합은 ' + cast(@added as varchar) + N'입니다.'
+		set @I = @I + 1
+	end
+		print @added
+print @added
+
+-- contiune, break
+declare @I int = 1 
+declare @added bigint = 0
+while @I <= 100
+	begin
+		if @I%9=0
+			begin 
+				print cast(@I as varchar) + N'은 9의 배수(더하기 진행 X)'
+				set @I = @I + 1
+				continue
+			end
+				-- set @I = @I + 1
+		if @added >= 3000
+			begin 
+				print N'@added값이 ' + cast(@added as varchar) + N'이고 3000을 넘어 계산 종료'
+				break
+			end
+		set @added = @added + @I
+		print N'현재 숫자는 ' + cast(@I as varchar) + N'이고 총 합은 ' + cast(@added as varchar) + N'입니다.'
+		set @I = @I + 1
+	end
+		print N'최종결과: ' + cast(@added as varchar);
+
+-- 동적 SQL
+create proc SP_SELECT_TABLE_INFO
+@TableName varchar(3000)
+as 
+begin
+	declare @sqlquery varchar(3000)
+	set @sqlquery = 'select * from ' + @tablename
+
+	exec(@sqlquery)
+end 
+go
+exec SP_SELECT_TABLE_INFO 'companyinfo';
+exec SP_SELECT_TABLE_INFO 'stockprice';
